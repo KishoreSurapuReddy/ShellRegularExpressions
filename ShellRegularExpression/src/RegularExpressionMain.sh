43a5cae61f622a0ca6691330eb69e4296b495ce6
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-13 16:23:20 +0530

    [kishorereddy] Refactor. rafactor the code with proper indentation in RegularExpressionMain.sh in src

--- ShellRegularExpression/src/RegularExpressionMain.sh
+++ ShellRegularExpression/src/RegularExpressionMain.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 #checking the user input data
 source RegularExpressionPatterns.sh
-firstNameValidation "Kishore"
-name=$?
-echo "first name"$name
-lastNameValidation "Reddy"
-last=$?
-echo "last name"$last
-userNameValidation "Kishore@123"
-user=$?
-echo "user nname "$user
-passwordValidation "Kishore@123"
-pass=$?
-echo "password"$pass
-numberValidation "91 1234567890"
+
+validateFirstName "Kishore"
+firstname=$?
+echo "first name is valid :"$firstname
+
+validateLastName "Reddy"
+lastname=$?
+echo "last name is valid :"$lastname
+
+validateUserName "Kishore@123"
+username=$?
+echo "user name is valid :"$username
+
+validatePassword "Kishore@123"
+password=$?
+echo "password is valid :"$password
+
+validateMobileNumber "91 1234567890"
 number=$?
-echo "nuber $number"
+echo "number is valid :$number"
+
+validateEMailId "kishore@gmail.com"
+mail=$?
+echo "mail is valid :$mail"
